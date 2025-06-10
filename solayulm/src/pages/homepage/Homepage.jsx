f868ff8777dcee1feb578245cff5f74be56c7bb3
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2025-06-10 22:35:28 +0100

    Update content to be center & only tag line

--- solayulm/src/pages/homepage/Homepage.jsx
+++ solayulm/src/pages/homepage/Homepage.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>Homepage</h1>
-    </div>
-  )
-}
+    <Container fluid className="d-flex flex-column">
+      <Row className="justify-content-left">
+        <Col className="text-center">
+          <h2>You've arrived! Welcome to the quiet edge of the galaxy.</h2>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default Homepage
+export default Homepage;
