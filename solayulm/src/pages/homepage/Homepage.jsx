03525977e85aedbeef3eb936092e002c95055914
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2025-06-11 00:04:06 +0100

    Add CTA button and text

--- solayulm/src/pages/homepage/Homepage.jsx
+++ solayulm/src/pages/homepage/Homepage.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const Homepage = () => {
   return (
     <Container fluid className="d-flex flex-column">
-      <Row className="justify-content-left">
+      <Row className="justify-content-start">
         <Col className="text-center">
           <h2>You've arrived! Welcome to the quiet edge of the galaxy.</h2>
         </Col>
+      </Row>
+      <Row className="justify-content-end" Style = {{display: "flex", flexWrap: "wrap" }}>
+        <Col className="text-right">
+          <h3>A peaceful digital space for you to rest and reflect or just float around for a while.</h3>
+        </Col>
+        <Col className="text-left">
+        <Button variant="dark" size='sm'>Begin sequence take off</Button>
+        </Col>
+
       </Row>
     </Container>
   );
