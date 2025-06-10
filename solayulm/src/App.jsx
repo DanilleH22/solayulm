fdb80ca4475ecabf3f04e9ba3fe86ad2d608e813
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2025-06-10 22:35:56 +0100

    Add navbar and container

--- solayulm/src/App.jsx
+++ solayulm/src/App.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage.jsx"
+import { Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from "./components/NavBar.jsx";
+import "./App.css";
+
+
 
 
 const App = () => {
   return (
+    <Container fluid>
+      <NavBar />
     <Router>
       <Routes>
         <Route exact path="/" element={<Homepage />} />
         <Route path="*" element={<h1>Page not found!</h1>} />
         </Routes>
     </Router>
+    </Container>
   );
 };
 
