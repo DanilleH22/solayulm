375286995da706aab7068f769515926f50439c7d
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2025-06-11 14:25:18 +0100

    Update layout to put footer at botttom

--- solayulm/src/App.jsx
+++ solayulm/src/App.jsx
@@ -4,6 +4,7 @@ import Homepage from "./pages/homepage/Homepage.jsx"
 import { Container } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from "./components/NavBar.jsx";
+import Footer from "./components/Footer.jsx";
 import "./App.css";
 
 
@@ -11,15 +12,18 @@ import "./App.css";
 
 const App = () => {
   return (
-    <Container fluid>
+    <div className="App">
       <NavBar />
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Homepage />} />
-        <Route path="*" element={<h1>Page not found!</h1>} />
-        </Routes>
-    </Router>
-    </Container>
+      <Router>
+        <div className="page-content">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="*" element={<h1>Page not found!</h1>} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </div>
   );
 };
 
