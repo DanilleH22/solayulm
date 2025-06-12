9e663ab8b19e31e92b7a52367ca9942c8ffead63
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2025-06-12 09:21:30 +0100

    Add more text for hero section

--- solayulm/src/pages/homepage/Homepage.jsx
+++ solayulm/src/pages/homepage/Homepage.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
 
 
 const Homepage = () => {
   return (
     <Container fluid className="d-flex flex-column">
-      <Row className="justify-content-start">
+      <Row className="justify-content-start p-3">
         <Col className="text-center">
           <h2>You've arrived! Welcome to the quiet edge of the galaxy.</h2>
         </Col>
       </Row>
-      <Row className="justify-content-end" Style = {{display: "flex", flexWrap: "wrap" }}>
+      <Row className="justify-content-end p-4" Style = {{display: "flex", flexWrap: "wrap" }}>
         <Col className="text-right">
-          <h3>A peaceful digital space for you to rest and reflect or just float around for a while.</h3>
-          {/* longer description of what it iis needed  */}
+          <h4>A peaceful digital space for you to rest and reflect or just float around for a while.</h4>
+          <p>Solayulm is your serene pocket of the universe. A digitl escape where calm meets the cosmos. Whether you are here to unwind, write a quiet though or simply float for a while, you've arrived to the perfect place. The queiet edge of the galaxy. No noise. No pressure. Just a gentle space to breathe.</p>
         </Col>
-        <Col className="text-left">
-        <Button variant="dark" size='sm'>Begin sequence take off</Button>
+        <Col className="text-left d-flex justify-content-center">
+        <Button variant="dark" style={{ width: "200px", height: "60px" }} >Step into Stillness</Button>
         </Col>
       </Row>
       <Row>
