a1b9b193fb531ae1e66bcf65aefc8e3e9e84642f
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2025-08-15 12:11:45 +0100

    Create space card to name room themes component

---
+++ solayulm/src/components/SpaceCard.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { motion } from "framer-motion";
+import "../styles/SpaceCard.css"; // Ensure this path is correct
+import Card from 'react-bootstrap/Card';
+
+const SpaceCard = ({ title, description }) => {
+  return (
+    <motion.div
+      className="space-card"
+      initial={{ y: 0 }}
+      animate={{ y: [0, -5, 0], rotate: [0, 0.5, 0] }} // floating effect
+      transition={{
+        duration: 6,
+        repeat: Infinity,
+        ease: "easeInOut",
+      }}
+    >
+      <Card
+        className="space-card-bg"
+        
+      >
+        <div className="light-sweep"></div>
+        <div className="space-card-content">
+          <h5>{title}</h5>
+          <p>{description}</p>
+        </div>
+      </Card>
+    </motion.div>
+  );
+};
+
+export default SpaceCard;
