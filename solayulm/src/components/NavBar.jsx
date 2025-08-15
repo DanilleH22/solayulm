0e4bafe96f0572ebde3af3ad5ec3253abc5b2708
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2025-08-15 09:48:04 +0100

    Add Type animation for navbar

--- solayulm/src/components/NavBar.jsx
+++ solayulm/src/components/NavBar.jsx
@@ -1,30 +1,39 @@
 import React from "react";
 import { Navbar, Container } from "react-bootstrap";
+import { TypeAnimation } from "react-type-animation";
 
-
-/**
- * Navigation bar.
- */
 const NavBar = () => {
-  
-
   return (
-    <Navbar className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">
-            {/* <img
-              alt=""
-              src="/img/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{' '} */}
-            <strong>Solayulm</strong>
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-  );
-}
+    <Navbar className="py-3">
+      <Container className="d-flex justify-content-between align-items-center">
+        {/* Left side */}
+        <Navbar.Brand href="#home">
+          <strong style={{ fontSize: "1.5rem" }}>Solayulm</strong>
+        </Navbar.Brand>
 
+        {/* Right side */}
+        <div className="d-flex justify-content-end">
+          <TypeAnimation
+            preRenderFirstString={true}
+            sequence={[
+              500,
+              "Comfort",
+              1000,
+              "Peace",
+              1000,
+              "Serenity",
+              1000,
+              "Release",
+              500,
+            ]}
+            speed={50}
+            style={{ fontSize: "1.5rem"}}
+            repeat={Infinity}
+          />
+        </div>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavBar;
