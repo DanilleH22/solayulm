48292e093fe448562086d1bbb9f3b37f40e79b11
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2025-09-04 11:16:16 +0100

    Create a visual sequence with flower of life, decompression loop & spiral loop

---
+++ src/components/VisualSequence.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { SpiralLoop } from "./SpiralLoop";
+import DecompressionLoop from "./DecompressionLoop.jsx";
+import FlowerOfLifeLoop from "./FlowerOfLifeLoop.jsx"
+import { easeInOut } from "framer-motion";
+
+const visuals = [
+  SpiralLoop,
+  DecompressionLoop,
+  FlowerOfLifeLoop,
+];
+
+export default function VisualSequence({ interval = 5000 }) {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIndex((prev) => (prev + 1) % visuals.length);
+    }, 20000);
+
+    return () => clearInterval(timer);
+  }, [interval]);
+
+  const CurrentVisual = visuals[index];
+  return <CurrentVisual transition={{ easeInOut }}/>;
+}
