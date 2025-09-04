13f2e27a322947d266cd0432168fa3f812c9851f
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2025-09-04 22:28:15 +0100

    Add sub text about wellbeing huddle & update stress and calm slider to have label & re organise wellbeing huddle layout & style energy slier and add label & addfocus dial

--- src/pages/homepage/DecompressionBay.jsx
+++ src/pages/homepage/DecompressionBay.jsx
@@ -5,11 +5,14 @@ import CustomCountdownTimer from "../../components/CustomCountdownTimer.jsx";
 import Slider from '@mui/material/Slider';
 import Stack from '@mui/material/Stack';
 import DecompressionLoop from "../../components/DecompressionLoop.jsx"; 
-import  FlowerOfLifeLoop  from "../../components/FlowerLifeLoop.jsx";
+import  FlowerLifeLoop  from "../../components/FlowerLifeLoop.jsx";
 import { SpiralLoop } from "../../components/SpiralLoop.jsx";
 import { NebulaFlow } from "../../components/NebulaFlow.jsx";
 import   JumpingDots  from "../../components/JumpingDots.jsx";
 import { RotatingRings } from "../../components/RotatingRings.jsx";
+import  MoodBar  from "../../components/MoodBar.jsx";
+import  FocusDial  from "../../components/FocusDial.jsx";
+import { Link } from "react-router";
 
 
 const DecompressionBay = () => {
@@ -19,6 +22,10 @@ const DecompressionBay = () => {
 function getAriaValueText(value) {
   return `${value}°C`;
 }
+
+const [focus, setFocus] = useState(50);
+
+
 
 
   return (
@@ -62,7 +69,14 @@ function getAriaValueText(value) {
         </Button>
         
       </div>
-      
+         <Row className="mt-4 pt-5">
+  <h3 className="text-center">Mood Spectrum Bar</h3>
+  <Col xs={12} className="d-flex justify-content-center">
+    <div className="d-flex justify-content-center"> 
+      <MoodBar />
+    </div>
+  </Col>
+</Row>
         <a
         className="mt-3 pt-6"
         onClick={() => setShowVisuals((prev) => !prev)} 
@@ -80,128 +94,259 @@ function getAriaValueText(value) {
 
      <Row className="text-center my-5 pb-5 h-75">
     
-        {/* Left - Environment */}
-    <Col md={4}>
-      <h3>Environment Controls</h3>
-      <Button>Ambient Sound</Button>
-      <Button>Lighting</Button>
-      <NebulaFlow />
-    </Col>
+        
 
-    {/* Right - Stress Bar */}
-    <Col md={8}>
+    
+    <Col md={12}>
     <h2 className="mb-3 mx-5">Well-being Huddle</h2>
+    <h4 className="mb-4 pb-4">Feel decompressed ? Let's do some reflection...</h4>
+    
       
-      <Row className="mb-4 pb-5 ">
-        <Col className="pr-5">
+      <Row>
+    <Col md={4}>
         <h3>Stress Levels</h3>
         <p>Adjust your current stress levels</p>
-       <Stack sx={{  width: 300 }} spacing={1} direction="row">
-      <Slider
-        aria-label="Stress levels"
-        orientation="horizontal"
-        getAriaValueText={getAriaValueText}
-        valueLabelDisplay="auto"
-        defaultValue={30}
-        sx={{
-    '& .MuiSlider-valueLabel': {
-      minWidth: 45,     
-      height: 32,
-      fontSize: 14,
-    },
-  }}
-      />
-      </Stack>
-        </Col>
-        <Col className="ml-5">
-        < RotatingRings />
-        </Col>
-
-        <Col>
-        <h3>Calm 'O' meter</h3>
+    </Col>
+    <Col md={4}>
+    <h3>Calm 'O' meter</h3>
         <p>Adjust your current calm levels</p>
-       <Stack sx={{width: 300 }} spacing={1} direction="row">
-      <Slider
-        aria-label="Calm 'O' meter"
-        orientation="horizontal"
-        getAriaValueText={getAriaValueText}
-        valueLabelDisplay="auto"
-        defaultValue={30}
-        sx={{
-    '& .MuiSlider-valueLabel': {
-      minWidth: 45,     
-      height: 32,
-      fontSize: 14,
-    },
-  }}
-      />
-      </Stack>
-        </Col>
-      </Row>
+    </Col>
+</Row>
 
-      <Row >
-        <Col md={4}>
-        <h3>Energy Guage</h3>
-        <Stack sx={{  width: 300 }} spacing={1} direction="row">
-      <Slider
-        aria-label="Stress levels"
-        orientation="horizontal"
-        getAriaValueText={getAriaValueText}
-        valueLabelDisplay="auto"
-        defaultValue={30}
-        sx={{
-    '& .MuiSlider-valueLabel': {
-      minWidth: 45,     
-      height: 32,
-      fontSize: 14,
-    },
-  }}
-      />
-      </Stack>
-        </Col>
-
-        <Col md={6}>
-        <h3>Focus Dials</h3>
-        </Col>
-      </Row>
-{/* mood spectrum section  */}
-      <Row>
-        <h3 className="mt-4 pt-5 text center">Mood Spectrum Bar</h3>
-        <Col className="pt-2 d-flex justify-content-center">
-        
-         <Slider
-  defaultValue={30}
-  aria-label="Mood Spectrum"
-  valueLabelDisplay="auto"
-  sx={{
-    height: 18,
+        <Row>
+  <Col className="d-flex justify-content-center pt-4 "
+  md={4}
+  style={{   transform: 'translate( 45%, -20px)'  }}>
+    {/* Stress levels */}
+    <Stack sx={{ height: 250 }}>
+      <Slider 
+      valueLabelDisplay="auto"
+      orientation="vertical" 
+      defaultValue={80}  
+      sx={{
+    
     '& .MuiSlider-rail': {
       opacity: 1, 
-      backgroundImage: 'linear-gradient(90deg, #FF0000, #FFFF00, #00FF00, #00FFFF, #0000FF, #FF00FF)',
+      backgroundImage: 'linear-gradient(90deg, #FF0000)',
       borderRadius: 6,
+      width: 10
     },
     '& .MuiSlider-track': {
-      backgroundColor: 'transparent', 
+      backgroundColor: ' #e39f9fff', 
+       border: 'none',
     },
     '& .MuiSlider-thumb': {
-      width: 40,
-      height: 40,
+      width: 30,
+      height: 30,
       backgroundColor: '#fff',
       border: '2px solid #000',
     },
+ '& .MuiSlider-valueLabel': {
+        lineHeight: 1.2,
+        fontSize: 12,
+        background: 'unset',
+        padding: 0, 
+        width: 32,
+        height: 32,
+        borderRadius: '50% 50% 50% 0', 
+        backgroundColor: '#e39f9fff',
+        transformOrigin: 'left center',
+        transform: 'translate(120%, -80%) rotate(-145deg) scale(0)',
+
+        '&::before': { display: 'none' },
+
+        '&.MuiSlider-valueLabelOpen': {
+          transform: 'translate(60%, -50%) rotate(-120deg) scale(1)',
+        },
+
+        '& > *': {
+          transform: 'rotate(45deg)', 
+        },
+      },
+  }}/>
+    </Stack>
+  </Col>
+  <Col className="d-flex justify-content-center pt-4"
+  md={4}
+  style={{   transform: 'translate( 45%, -20px)'  }}>
+    {/* Calm o meter */}
+    <Stack sx={{ height: 250 }}>
+  <Slider
+    valueLabelDisplay="auto"
+    orientation="vertical"
+    defaultValue={50}
+    sx={{
+      '& .MuiSlider-rail': {
+        opacity: 1,
+        backgroundImage: 'linear-gradient(90deg, #475fe8ff)',
+        borderRadius: 6,
+        width: 10,
+      },
+      '& .MuiSlider-track': {
+        backgroundColor: '#a8b2ebff',
+        border: 'none',
+      },
+      '& .MuiSlider-thumb': {
+        width: 30,
+        height: 30,
+        backgroundColor: '#fff',
+        border: '2px solid #000',
+        boxShadow: 'none',
+        '&:hover, &.Mui-focusVisible, &.Mui-active': {
+          boxShadow: 'none',
+        },
+      },
+      '& .MuiSlider-valueLabel': {
+        lineHeight: 1.2,
+        fontSize: 12,
+        background: 'unset',
+        padding: 0, 
+        width: 32,
+        height: 32,
+        borderRadius: '50% 50% 50% 0', 
+        backgroundColor: '#a8b2ebff',
+        transformOrigin: 'left center',
+        transform: 'translate(120%, -80%) rotate(-145deg) scale(0)',
+
+        '&::before': { display: 'none' },
+
+        '&.MuiSlider-valueLabelOpen': {
+          transform: 'translate(60%, -50%) rotate(-120deg) scale(1)',
+        },
+
+        '& > *': {
+          transform: 'rotate(45deg)', 
+        },
+      },
+    }}
+  />
+</Stack>
+
+  </Col>
+   <Col className="ml-5"style={{   transform: 'translate( -5%, -80px)'  }}  >
+        < RotatingRings />
+        </Col>
+
+</Row>
+
+      <Row >
+        <Col md={6}>
+        <h3>Energy Guage</h3>
+        </Col>
+        <Col md={6}>
+        <h3>Focus Dials</h3>
+        </Col>
+        </Row>
+        <Row>
+        <Col md={6}
+        className="d-flex justify-content-center pt-4"
+        >
+        
+        <Stack sx={{  width: 400 }} spacing={1} direction="row">
+      <Slider
+        aria-label="Stress levels"
+        orientation="horizontal"
+        getAriaValueText={getAriaValueText}
+        valueLabelDisplay="auto"
+        defaultValue={30}
+        className=""
+        style={{top: '70%'}}
+        sx={{
+
+            '& .MuiSlider-rail': {
+        opacity: 1,
+          backgroundImage: 'linear-gradient(90deg, #dd5f5fff, #5f70ddff)',
+                
+        borderRadius: 6,
+       
+      },
+      '& .MuiSlider-track': {
+        backgroundColor: 'transparent',
+        border: 'none',
+      },
+      '& .MuiSlider-thumb': {
+        width: 30,
+        height: 30,
+        backgroundColor: '#fff',
+        border: '2px solid #000',
+        boxShadow: 'none',
+       
+      },
+    '& .MuiSlider-valueLabel': {
+      minWidth: 45,     
+      height: 32,
+      fontSize: 14,
+    },
+    '& .MuiSlider-valueLabel': {
+    lineHeight: 1.2,
+    fontSize: 12,
+    background: 'unset',
+    padding: 0,
+    width: 32,
+    height: 32,
+    borderRadius: '50% 50% 50% 0',
+    backgroundColor: '#52af77',
+    transformOrigin: 'bottom left',
+    transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
+    '&::before': { display: 'none' },
+    '&.MuiSlider-valueLabelOpen': {
+      transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
+    },
+    '& > *': {
+      transform: 'rotate(45deg)',
+    }, }
   }}
-/>
+      />
+      </Stack>
+        </Col>
 
 
-            </Col>
+
+
+        <Col md={6}>
+        <div>
+    <FocusDial value={focus} />
+    <Slider
+      value={focus}
+      onChange={(e, val) => setFocus(val)}
+      orientation="horizontal"
+      sx={
+        {
+             '& .MuiSlider-rail': {
+        opacity: 1,
+          backgroundImage: 'linear-gradient(90deg, #e1b6faff )',
+                
+        borderRadius: 6,
+       
+      },
+      '& .MuiSlider-track': {
+        backgroundColor: 'transparent',
+        border: 'none',
+      },
+            
+        }
+      }
+    />
+  </div>
+        </Col>
       </Row>
+{/* mood spectrum section  */}
+     
 
     </Col>
 
     
   </Row>
 
-
+<Row>
+    <Col md={12}>
+    <div className="flex justify-content-center align-items-center" >
+    <Link to="/Rooms" style={{ background: 'none', textDecoration: 'none'}}>
+      <Button className={styles.MinutesButton} style={{display: 'flex', justifyContent: 'center'}}>Exit</Button>
+      </Link>
+    </div>
+    </Col>
+</Row>
 
       
 
