fdfa6fa000bf1f0f1571e0807f3c3ae9a715fe34
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2026-01-23 01:01:05 +0000

    Try fix centering ext btn

--- src/pages/homepage/ObservationDeck.jsx
+++ src/pages/homepage/ObservationDeck.jsx
@@ -837,22 +837,6 @@ const [show, setShow] = useState(false);
         </div>
       </div>
 
-      <Row>
-          <Col md={12}>
-          <div className="flex justify-content-center align-items-center" >
-          <Link to="/Rooms" style={{ textDecoration: 'none'}}>
-            <button  style={{display: 'flex', justifyContent: 'center', fontSize: '20px',
-    color: 'white',
-    cursor: 'pointer',
-   
-    justifySelf: 'center',
-    backgroundColor: '#ffffff1a' ,
-   width: window.innerWidth < 768 ? '30%' : '10%',
-    margin: '8px'}}>Exit</button>
-            </Link>
-          </div>
-          </Col>
-      </Row>
       
      <Row className="justify-content-center">
   <Col xs={12} className="text-center">
